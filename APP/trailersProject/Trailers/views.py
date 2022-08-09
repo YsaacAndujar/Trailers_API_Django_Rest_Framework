d12b11cc457efb2a3210f37b737f46eab5da7c41
Tyrone09876543 <rubik09876543@gmail.com>
2022-08-09 16:13:10 -0400

    fixed som details, just need to make the account page

--- APP/trailersProject/Trailers/views.py
+++ APP/trailersProject/Trailers/views.py
@@ -1,6 +1,8 @@
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from .models import CategoryForm, TrailersForm, Category, Trailers
+from django.db.models import Case, F, FloatField, Value, When, Value
 # Create your views here.
 def addTrailer(request):
     context = {}
@@ -16,7 +18,6 @@ def addTrailer(request):
                     return render(request, "trailers/add-trailer.html",context)
                 form.save()
                 context["valid"] = True
-                #context["form"] = TrailersForm()
                 return redirect(reverse('Trailers:addTrailer') + '?valid=True')
             except Exception as e:
                 context["error"]=True
@@ -62,17 +63,94 @@ def addCategory(request):
     return render(request, "trailers/add-category.html",context)
 
 def viewTrailers(request):
-    trailers = Trailers.objects.all()
-    context = {
-        "trailers":trailers
-    }
+    context = { }
+    trailers = Trailers.objects.all().order_by("-id")
+    try:
+        context["id"]=request.GET["id"]
+        trailers = trailers.filter(id=context["id"])
+    except:
+        pass
+    try:
+        context["category"]=request.GET["category"]
+        trailers = trailers.filter(category__in=Category.objects.filter(id=context["category"]))
+    except:
+        pass
+    try:
+        context["title"]=request.GET["title"]
+        trailers=trailers.filter(title__icontains=context["title"])
+        trailers = trailers.annotate(
+            k1 = Case(
+                When(title__icontains=context["title"], then=Value(1.0)),
+                default=Value(0.0),
+                output_field=FloatField(),
+            ),
+            k2 = Case(
+                When(title=context["title"], then=Value(1.0)),
+                default=Value(0.0),
+                output_field=FloatField(),
+            ),
+            rank= F("k1")+ F("k2"),
+        ).order_by("-rank")
+    except Exception as e:
+        pass
+    try:
+        context["from"]=request.GET["from"]
+        trailers = trailers.filter(release_date__gte=context["from"])
+    except:
+        pass
+    try:
+        context["to"]=request.GET["to"]
+        trailers = trailers.filter(release_date__gte=context["to"])
+    except:
+        pass
+    
+    paginator = Paginator(trailers, 10)
+    page = 1
+    try:
+        page = request.GET.get('page')
+    except:
+        pass
+    trailers = paginator.get_page(page)
+    categories = Category.objects.all()
+    context["categories"]=categories
+    context["trailers"]=trailers
+    
     return render(request, "trailers/view-trailers.html",context)
 
 def viewCategories(request):
-    categories = Category.objects.all()
-    context = {
-        "categories":categories
-    }
+    categories = Category.objects.all().order_by("-id")
+    context = {}
+    try:
+        context["id"]=request.GET["id"]
+        categories = categories.filter(id=context["id"])
+    except:
+        pass
+    try:
+        context["name"]=request.GET["name"]
+        categories=categories.filter(name__icontains=context["name"])
+        categories = categories.annotate(
+            k1 = Case(
+                When(name__icontains=context["name"], then=Value(1.0)),
+                default=Value(0.0),
+                output_field=FloatField(),
+            ),
+            k2 = Case(
+                When(name=context["name"], then=Value(1.0)),
+                default=Value(0.0),
+                output_field=FloatField(),
+            ),
+            rank= F("k1")+ F("k2"),
+        ).order_by("-rank")
+    except Exception as e:
+        pass
+    paginator = Paginator(categories, 10)
+    page = 1
+    try:
+        page = request.GET.get('page')
+    except:
+        pass
+    categories = paginator.get_page(page)
+    context["categories"]=categories
     return render(request, "trailers/view-categories.html",context)
 
 def modifyCategory(request, pk):
@@ -80,7 +158,9 @@ def modifyCategory(request, pk):
         category = Category.objects.get(id=pk)
     except Category.DoesNotExist:
         return redirect("Trailers:viewCategories")
-    context = {}
+    context = {
+        "category":category
+    }
     if request.method == "POST":
         form = CategoryForm(request.POST, instance=category)
         if form.is_valid():
@@ -96,9 +176,20 @@ def modifyCategory(request, pk):
             context["form"]=form
             context["error"]=True
             context["errorMessage"]="The form is not valid"
+        return render(request,"trailers/modify-category.html",context)
+
     else:
         form = CategoryForm(instance=category)
         context["form"] = form
+
+        try:
+            error = request.GET["error"]
+            if error:
+                context["error"]=True
+                context["errorMessage"]=request.GET["errorm"]
+        except:
+            pass
+
     return render(request,"trailers/modify-category.html",context)
 
 def modifyTrailer(request, pk):
@@ -133,12 +224,27 @@ def modifyTrailer(request, pk):
     else:
         form = TrailersForm(instance=trailer)
         context["form"]=form
+        try:
+            error = request.GET["error"]
+            if error:
+                context["error"]=True
+                context["errorMessage"]=request.GET["errorm"]
+        except:
+            pass
     return render(request, "trailers/modify-trailer.html",context)
 
 def deleteTrailer(request, pk):
     try:
         trailer = Trailers.objects.get(id=pk)
-    except Trailers.DoesNotExist:
-        return redirect("Trailers:viewTrailers")
-    trailer.delete()
+        trailer.delete()
+    except Exception as e:
+        return redirect(reverse("Trailers:modifyTrailers",args=(pk,))+ '?error=True&errorm='+e)
     return redirect("Trailers:viewTrailers")
+
+def deleteCategory(request, pk):
+    try:
+        category = Category.objects.get(id=pk)
+        category.delete()
+    except Exception as e:
+        return redirect(reverse("Trailers:modifyCategory",args=(pk,))+ '?error=True&errorm='+e)
+    return redirect("Trailers:viewCategories")
